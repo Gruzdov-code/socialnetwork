21ccae31ba68b461b1ab212f16cc19a7a1f98b42
Gruzdov-code <gruzdov.code@gmail.com>
2022-02-05 19:25:55 +0500

    Start

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="https://img2.freepng.ru/20180606/ryo/kisspng-thumb-signal-basingstoke-clip-art-5b185bcbc0df63.10493492152832301979.jpg"></img>
+      </header>
+      <div className="container">
+        <nav className="nav">
+          <div className="nav-ul">
+            <li className="nav-ul-li">
+              <span>tetert5tet</span>
+            </li>
+            <li className="nav-ul-li">
+              {" "}
+              <span>tetert5tet</span>
+            </li>
+            <li className="nav-ul-li">
+              <span>tetert5tet</span>
+            </li>
+            <li className="nav-ul-li">
+              {" "}
+              <span>tetert5tet</span>
+            </li>
+            <li className="nav-ul-li">
+              {" "}
+              <span>tetert5tet</span>
+            </li>
+            <li className="nav-ul-li">
+              {" "}
+              <span>tetert5tet</span>
+            </li>
+            <li className="nav-ul-li">
+              {" "}
+              <span>tetert5tet</span>
+            </li>
+            <li className="nav-ul-li">
+              {" "}
+              <span>tetert5tet</span>
+            </li>
+          </div>
+        </nav>
+        <div className="main-content">
+          <img src="https://pbs.twimg.com/profile_banners/1153728498688827392/1563908974/1500x500"></img>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
